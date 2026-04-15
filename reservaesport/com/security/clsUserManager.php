68b3294829669350498de23d1bffd6a38a983002
bydani22 <dani.martin.rojas22@gmail.com>
2026-04-15 18:34:20 +0200

    5

--- reservaesport/com/security/clsUserManager.php
+++ reservaesport/com/security/clsUserManager.php
@@ -50,58 +50,7 @@ class UserManager
         }
     }
 
-    public function recoverPasswordEmail($email)
-    {
-        try {
-            $result = $this->dbCommand->execute('sp_wdev_recover_password_email', array($email));
-            $xml = simplexml_load_string($result);
-            if ($xml->ws_response->head->errors->error->num_error == "0") {
-                $username = $xml->USERNAME;
-                $recovery_code = $xml->RECOVERY_CODE;
-
-                // URL del Web App desplegado en Google Apps Script
-                //url pau
-                // $url = 'https://script.google.com/macros/s/AKfycbzs-WaweIA_cKNVVgqqPmianx7dn4wPI7AflDvM78iUcP8pUoYNh5u5Dg7nBlkofdKu/exec';
-
-                //url Pol
-                $url = 'https://script.google.com/macros/s/AKfycbxAQsgiFCg31C-G1MzD27GjZTo0Owa22XBoGJQzu2AT-WV8lWj76kud2WOuxLaxpH6OYw/exec';
-
-                // Parámetros del correo electrónico
-                $destinatario = $email;
-                $asunto = 'Recuperar contraseña.';
-                $cuerpo = $username . ', su código de recuperación de contraseña es ' . $recovery_code . '.';
-                $adjunto = null;
-
-                // Llamada a la función para enviar el correo
-                enviarCorreo($url, $destinatario, $asunto, $cuerpo, $adjunto);
-            }
-            //Establecer el encabezado para XML
-            header('Content-Type: text/xml');
-
-            // Mostrar la respuesta XML
-            echo $xml->asXML();
-
-        } catch (PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
-        }
-    }
-
-    public function recoverPasswordPIN($email, $pin, $password)
-    {
-        try {
-            $result = $this->dbCommand->execute('sp_wdev_recover_password_pin', array($email, $pin, $password));
-            $xml = simplexml_load_string($result);
-            //Establecer el encabezado para XML
-            header('Content-Type: text/xml');
-
-            // Mostrar la respuesta XML
-            echo $xml->asXML();
-
-        } catch (PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
-        }
-    }
-
+   
     public function login($username, $password)
     {
         if (empty($username) || empty($password)) {
@@ -130,20 +79,7 @@ class UserManager
         }
     }
 
-    public function changePassword($ssid, $password, $newpassword)
-    {
-        try {
-            $result = $this->dbCommand->execute('sp_user_change_password', array($ssid, $password, $newpassword));
-
-            // Establecer el encabezado para XML
-            header('Content-Type: text/xml');
-
-            // Mostrar la respuesta XML
-            echo $result;
-        } catch (PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
-        }
-    }
+   
 
     public function accountValidate($username, $code)
     {
@@ -165,78 +101,8 @@ class UserManager
         }
     }
 
-    public function listusers($ssid)
-    {
-        if (empty($ssid)) {
-            echo "Todos los campos son obligatorios.";
-        } else {
-            try {
-                $result = $this->dbCommand->execute('sp_list_users2', array($ssid));
 
-                // Establecer el encabezado para XML
-                header('Content-Type: text/xml');
-
-                // Mostrar la respuesta XML
-                echo $result;
-
-            } catch (PDOException $e) {
-                echo 'Error: ' . $e->getMessage();
-            }
-        }
-    }
-
-    public function checkpwd($pwd)
-    {
-        if (!isset($_GET['pwd'])) {
-            echo "<span style='color: red;'>❌ No se recibió ninguna contraseña.</span>";
-            return;
-        }
-
-        try {
-            // Llamar a la procedure sp_check_pwd
-            $sql = "DECLARE @score INT, @xml XML;
-                EXEC sp_check_pwd :pwd, @score OUTPUT, @xml OUTPUT;
-                SELECT @score AS score, @xml AS xml;";
-
-            $result = $this->dbCommand->execute2($sql, array($pwd));
-
-            // Obtener el resultado
-            $result = $result->fetch(PDO::FETCH_ASSOC);
-            $score = $result['score'];
-            $xml = $result['xml'];
-
-            // Definir el mensaje según la puntuación
-            $message = "";
-            if ($score <= 10) {
-                $message = "<span style='color: red;'>❌ Muy débil<br>Falta: " . htmlspecialchars($xml) . "</span>";
-            } elseif ($score <= 20) {
-                $message = "<span style='color: orange;'>⚠️ Débil<br>Falta: " . htmlspecialchars($xml) . "</span>";
-            } elseif ($score <= 40) {
-                $message = "<span style='color: yellow;'>🟡 Aceptable<br>Falta: " . htmlspecialchars($xml) . "</span>";
-            } else {
-                $message = "<span style='color: green;'>✅ Fuerte</span>";
-            }
-
-            echo $message;
-        } catch (Exception $e) {
-            echo "<span style='color: red;'>⚠️ Error en la validación: " . $e->getMessage() . "</span>";
-        }
-    }
-    public function blockuser($ssid)
-    {
-        if (empty($ssid)) {
-            echo "Todos los campos son obligatorios.";
-        } else {
-            try {
-                    $result = $this->dbCommand->execute('sp_block_user', array ($ssid));
-                    $xml = simplexml_load_string($result);
-                    header('Content-Type: text/xml');
-                    echo $xml->asXML();
-            } catch (PDOException $e) {
-                echo 'Error: ' . $e->getMessage();
-            }
-        }
-    }
+   
 
     public function reservar($ssid)
     {
@@ -254,13 +120,13 @@ class UserManager
         }
     }
 
-    public function reservar($ssid)
+    public function fecha($ssid)
     {
         if (empty($ssid)) {
             echo "Todos los campos son obligatorios.";
         } else {
             try {
-                    $result = $this->dbCommand->execute('sp_block_user', array ($ssid));
+                    $result = $this->dbCommand->execute('sp_fecha', array ($ssid));
                     $xml = simplexml_load_string($result);
                     header('Content-Type: text/xml');
                     echo $xml->asXML();
@@ -270,7 +136,21 @@ class UserManager
         }
     }
 
-
+public function aleta($ssid)
+    {
+        if (empty($ssid)) {
+            echo "Todos los campos son obligatorios.";
+        } else {
+            try {
+                    $result = $this->dbCommand->execute('sp_reserevar', array ($ssid));
+                    $xml = simplexml_load_string($result);
+                    header('Content-Type: text/xml');
+                    echo $xml->asXML();
+            } catch (PDOException $e) {
+                echo 'Error: ' . $e->getMessage();
+            }
+        }
+    }
 }
 
 ?>
